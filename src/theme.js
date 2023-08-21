0bff032de4ea6d6b0995d1f4baa5b38ddf19e551
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2023-08-22 01:35:05 +0300

    ...

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
-import { colors } from "@mui/material";
 //color design tokens
 export const tokens = (mode) => ({
   ...(mode === "dark"
